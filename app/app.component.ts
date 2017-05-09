c92660c60e85bce13b97d377865d75af833ffd53
andela-wcyn <cynthia.abura@andela.com>
2017-05-09 23:34:25 +0300

    [Feature] Add Material Design template into project

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {LocationStrategy, PlatformLocation, Location} from '@angular/common';
+
+declare var $:any;
 
 @Component({
     selector: 'bl-app',
-    template: `
-        <h1>{{ pageTitle }}</h1>
-    `
+    moduleId: module.id,
+    templateUrl: 'app.component.html'
 })
-export class AppComponent { 
-    pageTitle: string = "The Bucket List"
+
+export class AppComponent implements OnInit{
+    location: Location;
+    constructor(location:Location) {
+        this.location = location;
+    }
+    ngOnInit(){
+        $.getScript('../assets/js/material-dashboard.js');
+        $.getScript('../assets/js/initMenu.js');
+    }
+    public isMaps(path: string){
+        var titlee = this.location.prepareExternalUrl(this.location.path());
+        titlee = titlee.slice( 1 );
+        if(path == titlee){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 }
